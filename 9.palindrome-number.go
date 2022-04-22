a7b9632acf1debd7a9072cef47a44a5f40280c88
Adamlixi <675212976@qq.com>
2022-04-22 11:02:49 +0800

    测试Git

---
+++ 9.palindrome-number.go
@@ -0,0 +1,14 @@
+package leetcode
+
+/*
+ * @lc app=leetcode id=9 lang=golang
+ *
+ * [9] Palindrome Number
+ */
+
+// @lc code=start
+func isPalindrome(x int) bool {
+	return false
+}
+
+// @lc code=end
